3ab9908cb34182c033d5bb814edd757639600344
Gayan Madusanka Godawela Gamage <91845466+3GRuby@users.noreply.github.com>
2022-03-03 23:49:28 +0100

    setup environment

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Application } from "express";
+
+const app :Application = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+
+});
+
+app.listen(5000, () => console.log ('Server running'));
